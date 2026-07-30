52ce33366e4ce9e6ee1f0d2442b966d8fe67727e
DHANYA305 <dhanyabhandari30@gmail.com>
2026-07-31 01:51:05 +0530

    Fix API URL

--- public/provider.js
+++ public/provider.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
     const response = await fetch(
-      "http://localhost:5003/events",
+      "/events",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
